a5892147c2415763d8f1fbc3ff9a822ebc289a33
VastSea0 <vastseaoffical0@outlook.com>
2024-10-11 22:31:16 +0300

    Refactor firebase.js to import the correct Firebase module and remove unnecessary code

--- src/tests/mobile-web-app-kit/src/firebase/firebase.js
+++ src/tests/mobile-web-app-kit/src/firebase/firebase.js
@@ -1,5 +1,5 @@
 // firebase.js
-import firebase from 'firebase/compat/app'; // Doğru Firebase modülünü içe aktarın
+import firebase from 'firebase/compat/app'; 
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
@@ -15,13 +15,10 @@ const firebaseConfig = {
   };
   
   
-// Firebase'i başlatın
 firebase.initializeApp(firebaseConfig);
 
-// Diğer Firebase hizmetlerini de içe aktarın
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 const storage = firebase.storage();
 
-// firebase nesnesini de dışa aktarın
 export { auth, firestore, storage, firebase };
