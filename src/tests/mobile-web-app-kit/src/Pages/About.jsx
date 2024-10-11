2e2932fe07b9aa4685dbb89541cd449a1bb0ec4e
VastSea0 <vastseaoffical0@outlook.com>
2024-10-11 21:04:30 +0300

    Refactor About page to update project description

--- src/tests/mobile-web-app-kit/src/Pages/About.jsx
+++ src/tests/mobile-web-app-kit/src/Pages/About.jsx
@@ -7,8 +7,17 @@ export default function About(){
         <div className="card mt-4">
           <div className="card-body">
             <h5 className="card-title">About Us</h5>
-            <p className="card-text">Learn more about the Mobile Web App Kit.</p>
-          
+            <div className="card-text">
+              <p>
+                Translate Me! is a secondary version of the OpenSoda project, more advanced and written from scratch. It uses the same technologies as OpenSoda. The project is a language learning platform that allows users to learn new languages by translating texts.
+              </p>
+              <p>
+                Translate Me! is a language learning platform and an open source and free software project. To contribute to the project, you can improve language translations and source code.
+              </p>
+              <p>
+                This project also includes JavaScript test scripts in its source code to provide footnotes for other developers in the learning process. These test scripts and apps are specific scripts created directly from the project itself
+              </p>
+            </div>
           </div>
         </div>
       </div>
