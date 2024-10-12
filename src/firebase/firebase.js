ebbadfe7c56daaf53fc711be9bbc0677eebb9f08
VastSea0 <vastseaoffical0@outlook.com>
2024-10-12 15:47:42 +0300

    Refactor firebase.js to import the correct Firebase module and remove unnecessary code

---
+++ src/firebase/firebase.js
@@ -0,0 +1,24 @@
+// firebase.js
+import firebase from 'firebase/compat/app'; 
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
+  
+  
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const storage = firebase.storage();
+
+export { auth, firestore, storage, firebase };
