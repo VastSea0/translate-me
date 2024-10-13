2517eaed7829edd25aed2f18f38c347d9b243d28
VastSea0 <vastseaoffical0@outlook.com>
2024-10-14 01:06:30 +0300

    Refactor Loading component to display loading text

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-export default function Loading() {
-    return (
-        <div className="flex justify-center items-center min-h-screen">
+export default function Loading({ text }) {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="flex justify-center items-center mb-4">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
       </div>
-    );
+      <div className="text-center">
+        {text}...
+      </div>
+    </div>
+  );
 }
