c1aa059ad6474974ee40b9d420c626c256c286fd
VastSea0 <vastseaoffical0@outlook.com>
2024-10-19 16:49:00 +0300

    Refactor components to support dark mode and improve user experience

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { UserIsSignedIn } from "./PartsOfUser";
 
 export default function Loading({ text }) {
+  const user = UserIsSignedIn();
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      if (user.darkMode !== undefined) {
+        setDarkMode(user.darkMode);
+      }
+    }
+  }
+  , [user]);
+
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
+    <div className={`flex flex-col justify-center items-center min-h-screen ${darkMode ? 'bg-white text-black' : 'bg-gray-900 text-white' }`}>
       <div className="flex justify-center items-center mb-4">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
       </div>
-      <div className="text-center">
+      <div className={`text-center ${darkMode ? 'bg-white text-black' : 'bg-gray-900 text-white' }`}>
         {text}...
       </div>
     </div>
--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import {motion, AnimatePresence} from 'framer-motion';
+import { UserIsSignedIn } from "./PartsOfUser";
 
 export default function Message({message}) {
+    const [darkMode, setDarkMode] = useState(false);
+    const user = UserIsSignedIn();
+
+    useEffect(() => {
+        if (user) {
+            if (user.darkMode !== undefined) {
+                setDarkMode(user.darkMode);
+            }
+        }
+    }
+    , [user]);
+
     return (
  <>
             {message && (
@@ -11,7 +24,7 @@ export default function Message({message}) {
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -50 }}
                     >
-                        <div className="bg-white p-3 rounded-lg shadow-lg">
+                        <div className={`p-3 rounded-lg shadow-lg ${darkMode ? 'bg-white text-black' : 'bg-gray-900 text-white' }`}>
                             <p className="text-lg font-semibold">{message}</p>
                         </div>
                     </motion.div>
